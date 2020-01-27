95ebc7a811946957583830b8967524f548470e83
Alex <asergy34@gmail.com>
2020-01-27 10:10:23 +0300

    Copy in branch production

--- client/src/redux/index.js
+++ client/src/redux/index.js
@@ -1,12 +1,11 @@
 import { createStore, applyMiddleware } from 'redux'
 import createRootReducer from './reducer'
-import logger from 'redux-logger'
 import createSagaMiddleware from 'redux-saga'
 import rootSaga from './saga'
 
 const sagaMiddleware = createSagaMiddleware()
 const rootReducer = createRootReducer()
-const enhancer = applyMiddleware(sagaMiddleware, logger)
+const enhancer = applyMiddleware(sagaMiddleware)
 const store = createStore(rootReducer, enhancer)
 
 sagaMiddleware.run(rootSaga)
