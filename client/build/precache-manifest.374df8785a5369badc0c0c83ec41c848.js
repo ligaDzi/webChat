278d9107ae5386071db426948166d00bf0793208
Alex <asergy34@gmail.com>
2020-01-27 13:05:55 +0300

    Apdate config

---
+++ client/build/precache-manifest.374df8785a5369badc0c0c83ec41c848.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "585e02b6465d8ed290486a27c47c4f6f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3116d4b6bf7163d0b168",
+    "url": "/static/css/main.e295c047.chunk.css"
+  },
+  {
+    "revision": "c1990ddd5c04bf52efb1",
+    "url": "/static/js/2.98869eb4.chunk.js"
+  },
+  {
+    "revision": "ae29ce3b7c74801ab49f5d5e9edab327",
+    "url": "/static/js/2.98869eb4.chunk.js.LICENSE"
+  },
+  {
+    "revision": "3116d4b6bf7163d0b168",
+    "url": "/static/js/main.deedc4b3.chunk.js"
+  },
+  {
+    "revision": "5536b1ffa5673e4eb964",
+    "url": "/static/js/runtime-main.d07f956b.js"
+  }
+]);
